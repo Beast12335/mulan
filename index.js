053385f18ffb3f8b07e5dc98b16dc57f04f17017
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2023-05-13 14:39:40 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,47 @@
+const lib = require('lib')({token: process.env['ltoken']});
+
+const { Client, GatewayIntentBits, Partials } = require('discord.js');
+
+const client = new Client({
+
+  intents: [
+
+    GatewayIntentBits.Guilds,
+
+    GatewayIntentBits.MessageContent,
+
+    GatewayIntentBits.GuildMessages,
+
+  ],
+
+});
+
+client.on("ready", ()=>console.log("READY"));
+
+client.on(Events.InteractionCreate, async interaction => {
+
+	if (interaction.isAutocomplete()) {		const command = interaction.client.commands.get(interaction.commandName);
+
+		if (!command) {
+
+			console.error(`No command matching ${interaction.commandName} was found.`);
+
+			return;
+
+		}
+
+		try {
+
+			await command.autocomplete(interaction);
+
+		} catch (error) {
+
+			console.error(error);
+
+		}
+
+	}
+
+});
+
+client.login(process.env['TOKEN']);
