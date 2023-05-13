660d32e59b8e256d2577c9c06ba51162cb6d4ffd
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2023-05-13 14:49:27 +0530

    Update command.js

--- command.js
+++ command.js
@@ -12,6 +12,26 @@ module.exports = {
 			.setDescription('Info about a match)
 
 			.addStringOption(option => option.setName('id').setDescription('Match Id').setAutocomplete(true)))
+		       async autocomplete(interaction) {
+
+		const focusedValue = interaction.options.getFocused();
+
+		const choices = ['Popular Topics: Threads', 'Sharding: Getting started', 'Library: Voice Connections', 'Interactions: Replying to slash commands', 'Popular Topics: Embed preview'];
+
+		const filtered = choices.filter(choice => choice.startsWith(focusedValue));
+
+		await interaction.respond(
+
+			filtered.map(choice => ({ name: choice, value: choice })),
+
+		);
+
+	},
+		
+
+	
+
+}
 		       }
 	
 	
