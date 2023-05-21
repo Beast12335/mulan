a64d322c6032a975a7d0a75d835bdad13b53ded8
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2023-05-21 12:39:23 +0530

    Create test.js

---
+++ commands/match/test.js
@@ -0,0 +1,26 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('test')
+    .setDescription('demo')
+    .addStringOption(option =>
+			option.setName('match')
+				.setDescription('Choose the match')
+				.setAutocomplete(true)),
+  async autocomplete(interaction) {
+    const focusedValue = interaction.options.getFocused();
+    const choices = ['Popular Topics: Threads', 'Sharding: Getting started', 'Library: Voice Connections', 'Interactions: Replying to slash commands', 'Popular Topics: Embed preview'];
+    const filtered = choices.filter(choice => choice.startsWith(focusedValue));
+    await interaction.respond(
+      filtered.map(choice => ({ name: choice, value: choice })),
+    );
+  },
+  async execute(interaction) {
+    try{
+        await interaction.reply('Pong!');
+    }catch(e){
+        console.log(e)
+    }
+  },
+};
