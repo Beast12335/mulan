884f669aeebbe2da63c31f5ec7e1e5bf753870c3
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2023-05-22 21:47:45 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,4 +1,16 @@
 const mysql = require('mysql2');
+const dotenv = require('dotenv');
+
+// Read the contents of the .env file
+const envFile = fs.readFileSync('.env');
+
+// Parse the contents into an object
+const envConfig = dotenv.parse(envFile);
+
+// Load the environment variables
+for (const key in envConfig) {
+  process.env[key] = envConfig[key];
+}
 
 // create the connection to database
 module.exports:{
