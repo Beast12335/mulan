1f567f2b96708322d3fd94e453761793ce3bf92a
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2023-05-23 11:55:45 +0530

    Create db.js

---
+++ db.js
@@ -0,0 +1,23 @@
+const mysql = require('mysql2');
+const dotenv = require('dotenv');
+
+// Read the contents of the .env file
+const envFile = fs.readFileSync('.env');
+
+// Parse the contents into an object
+const envConfig = dotenv.parse(envFile);
+
+// Load the environment variables
+for (const key in envConfig) {
+  process.env[key] = envConfig[key];
+}
+
+// create the connection to database
+module.exports.connection = {
+    async function connection (){
+        mysql.createConnection({
+            host: process.env['host'],
+            user: process.env['user'],
+            database: 'mulan'
+})
+}};
