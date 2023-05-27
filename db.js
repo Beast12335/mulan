d672db62044afe1b54b9bea7944eb7a7491cc759
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2023-05-27 11:43:28 +0530

    Create db.js

---
+++ db.js
@@ -0,0 +1,43 @@
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const fs = require('fs');
+const dotenv = require('dotenv');
+
+// Read the contents of the .env file
+const envFile = fs.readFileSync('.env');
+
+// Parse the contents into an object
+const envConfig = dotenv.parse(envFile);
+
+// Load the environment variables
+for (const key in envConfig) {
+  process.env[key] = envConfig[key];
+}
+const user = encodeURIComponent(process.env['user'])
+const password = encodeURIComponent(process.env['password'])
+                     
+const uri = `mongodb+srv://${user}:${password}@mulan.tncwlyu.mongodb.net/mulan?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+module.exports = run 
