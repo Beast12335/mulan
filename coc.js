65d1304aa160ff7a32a6682cbb477f73664b27ad
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2023-06-05 13:33:17 +0530

    Update coc.js

--- coc.js
+++ coc.js
@@ -1,4 +1,16 @@
 const {Client} = require('clashofclans.js');
+const dotenv = require('dotenv');
+const fs = require ('fs');
+// Read the contents of the .env file
+const envFile = fs.readFileSync('.env');
+
+// Parse the contents into an object
+const envConfig = dotenv.parse(envFile);
+
+// Load the environment variables
+for (const key in envConfig) {
+  process.env[key] = envConfig[key];
+}
 const client = new Client();
 const cc = await client.login({email: process.env.mail, password: process.env.password});
 module.exports = cc
