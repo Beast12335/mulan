ed069a7a1821c99d4e1218b702590852c20cd1f8
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2023-06-05 15:23:55 +0530

    Create player-claim.js

---
+++ commands/player/player-claim.js
@@ -0,0 +1,77 @@
+const {
+  SlashCommandBuilder,
+  EmbedBuilder,
+  PermissionsBitField,
+} = require('discord.js');
+const claim = require('../../db/claim.js');
+const cc = require('../../coc.js');
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('player-claim')
+    .setDescription('Claim a player')
+    .addStringOption((option) =>
+      option
+        .setName('tag')
+        .setDescription('Enter the player tag')
+        .setRequired(true)
+    )
+    .addStringOption((option) =>
+      option
+        .setName('token')
+        .setDescription('Enter the api token found in game')
+        .setRequired(true)
+    ),
+  async execute(interaction) {
+    await interaction.deferReply();
+    let search = interaction.getString('tag');
+    let regex = /^#[PYLQGRJCUV0289]+$/gm;
+    let token = interaction.getString('token');
+    try {
+      if (regex.test(search) == true) {
+        let claim = await cc.verifyPlayerToken(search, token);
+        if (claim == true) {
+          await claim.create({
+            user: interaction.user.id,
+            tag: search,
+          });
+          let embed = new EmbedBuilder()
+            .setColor(0xffff00)
+            .setTitle('')
+            .setDescription('Player sucessfully claimed.');
+          await interaction.followUp({
+            content: '',
+            embeds: [embed],
+          });
+        } else {
+          let embed = new EmbedBuilder()
+            .setColor(0xffff11)
+            .setTitle('Error')
+            .setDescription('Invalid API Token.');
+          await interaction.followUp({
+            content: '',
+            embeds: [embed],
+          });
+        }
+      } else {
+        let embed = new EmbedBuilder()
+          .setColor(0xffff11)
+          .setTitle('Error')
+          .setDescription('Invalid Player tag');
+        await interaction.followUp({
+          content: '',
+          embeds: [embed],
+        });
+      }
+    } catch (e) {
+      console.log(e);
+      let embed = new EmbedBuilder()
+        .setColor(0xffff11)
+        .setTitle('Error')
+        .setDescription(e);
+      await interaction.followUp({
+        content: '',
+        embeds: [embed],
+      });
+    }
+  },
+};
