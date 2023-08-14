465f0bb8b8b908a02e8115a58e7ed0bbd92f1533
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2023-08-14 21:47:58 +0530

    Create setup.js

---
+++ commands/misc/setup.js
@@ -0,0 +1,62 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { PermissionsBitFiels, ActionRowBuilder, ButtonBuilder, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('setup')
+    .setDescription('Set up tournament access buttons')
+    .addChannelOption(option =>
+      option.setName('channel')
+        .setDescription('Channel to send the setup message')
+        .setRequired(true)),
+  async execute(interaction) {
+    await interaction.deferReply() 
+    const userPermissions = interaction.member.permissions;
+    if (!userPermissions.has(PermissionsBitField.Flags.ADMINISTRATOR)) {
+      return interaction.followUp({ content: 'You don\'t have permission to use this command.', ephemeral: true });
+    }
+
+    const channel = interaction.options.getChannel('channel');
+
+    const th11Button = new ButtonBuilder()
+      .setCustomId('th11')
+      .setLabel('TH11')
+      .setStyle('SUCCESS');
+
+    const th12Button = new ButtonBuilder()
+      .setCustomId('th12')
+      .setLabel('TH12')
+      .setStyle('SUCCESS');
+
+    const th14Button = new ButtonBuilder()
+      .setCustomId('th14')
+      .setLabel('TH14')
+      .setStyle('SUCCESS');
+
+    const th15Button = new ButtonBuilder()
+      .setCustomId('th15')
+      .setLabel('TH15')
+      .setStyle('SUCCESS');
+
+    const mixCupButton = new ButtonBuilder()
+      .setCustomId('mix_cup')
+      .setLabel('Mix Cup')
+      .setStyle('SUCCESS');
+
+    const row = new ActionRowBuilder()
+      .addComponents(th11Button, th12Button, th14Button, th15Button, mixCupButton);
+
+    const embed = new EmbedBuilder()
+      .setTitle('Tournament Access Setup')
+      .setDescription('React on the buttons below to access tournaments')
+      .setColor('BLUE');
+
+    try {
+      await interaction.channel.send({ embeds: [embed], components: [row] });
+      await interaction.followUp({content:`Setup done`});
+    } catch (error) {
+      console.error(error);
+      interaction.followUp({ content: 'An error occurred while sending the setup message.', ephemeral: true });
+    }
+  },
+};
