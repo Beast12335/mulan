6e3f5986653bc3a40cb67b44e90294c32ee0eca0
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2023-08-14 22:18:05 +0530

    Update th11.js

--- commands/misc/th11.js
+++ commands/misc/th11.js
@@ -1,4 +1,4 @@
-const client = require ('././bot.js')
+const client = require ('../bot.js')
 module.exports = {
   name: 'interactionCreate',
   async execute(interaction) {
@@ -6,7 +6,9 @@ module.exports = {
     if (interaction.customId !== 'th11') return;
 
     try {
-
+      const member = interaction.user.id
+      const role = interaction.guild.roles.cache.find(x => x.id == `1140684315995418674`);
+      await member.roles.add(role)
       // Send an embed with two buttons to add or remove roles
       await interaction.followUp({ content:`You can now access the TH11 tournament.Someone from staff will associate with the bot soon.`, ephemeral:true});
 
