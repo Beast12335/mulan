cca32714a03dc880e726eb3f191d08384ca40263
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2023-08-15 16:20:20 +0530

    Update mix_cup.js

--- events/mix_cup.js
+++ events/mix_cup.js
@@ -1,20 +1,25 @@
-const client = require ('../index.js')
 module.exports = {
   name: 'interactionCreate',
   async execute(interaction) {
     if (!interaction.isButton()) return;
     if (interaction.customId !== 'mix_cup') return;
+    await interaction.deferReply({ephemeral:true})
 
     try {
-      const member = interaction.user.id
-      const role = interaction.guild.roles.cache.find(x => x.id == `1140684315995418674`);
-      await member.roles.add(role)
-      const channel = await client.guild.channels.cache.find(x => x.id == `1140617134913900545`)
-      await channel.send({content:`${interaction.user.username} wants to access Mix Cup tournaments.`});
-      // Send an embed with two buttons to add or remove roles
-      await interaction.followUp({ content:`You can now access the Mix Cup tournament.Someone from staff will associate with the bot soon.`, ephemeral:true});
-
-    } catch (error) {
+      const client = interaction.client;
+      const member = interaction.member
+      const role = interaction.guild.roles.cache.find(x => x.id == `1140684728278728876`);
+      const channel = await client.channels.fetch(`1140617134913900545`)
+      if (!member.roles.cache.some(x => x.id == `1140684728278728876`){
+        await member.roles.add(role)
+        await channel.send({content:`${interaction.user.username} wants to access Mix Cup tournaments.`});
+        await interaction.followUp({ content:`You can now access the Mix Cup tournament.Someone from staff will associate with the bot soon.`, ephemeral:true});
+      }
+      else{
+        await member.roles.remove(role)
+        await channel.send({content:`${interaction.user.username} removed Mix Cup Access.`});
+        await interaction.followUp({content:`Removed Mix Cup access from you.`, ephemeral:true});
+      }} catch (error) {
       console.error('Error handling mix cup button interaction:', error);
     }
   },
