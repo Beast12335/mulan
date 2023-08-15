837737541fb3cadfe7a2fc783704f216086a6223
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2023-08-15 16:25:04 +0530

    Update mix_cup.js

--- events/mix_cup.js
+++ events/mix_cup.js
@@ -10,7 +10,7 @@ module.exports = {
       const member = interaction.member
       const role = interaction.guild.roles.cache.find(x => x.id == `1140684728278728876`);
       const channel = await client.channels.fetch(`1140617134913900545`)
-      if (!member.roles.cache.some(x => x.id == `1140684728278728876`){
+      if (!member.roles.cache.some(x => x.id == `1140684728278728876`)){
         await member.roles.add(role)
         await channel.send({content:`${interaction.user.username} wants to access Mix Cup tournaments.`});
         await interaction.followUp({ content:`You can now access the Mix Cup tournament.Someone from staff will associate with the bot soon.`, ephemeral:true});
