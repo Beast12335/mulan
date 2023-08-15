1a4bba0a24e8546434fe369e6b7d6bc5eb0d2338
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2023-08-15 08:37:35 +0530

    Update th11.js

--- events/th11.js
+++ events/th11.js
@@ -8,12 +8,16 @@ module.exports = {
     try {
       const member = interaction.user.id
       const role = interaction.guild.roles.cache.find(x => x.id == `1140684315995418674`);
-      await member.roles.add(role)
       const channel = await client.guild.channels.cache.find(x => x.id == `1140617134913900545`)
-      await channel.send({content:`${interaction.user.username} wants to access TH11 tournaments.`});
+      if (!member.roles.cache.some(x => x.id == `1140684315995418674`)){
+        await member.roles.add(role)
+        await channel.send({content:`${interaction.user.username} wants to access TH11 tournaments.`});
       // Send an embed with two buttons to add or remove roles
-      await interaction.followUp({ content:`You can now access the TH11 tournament.Someone from staff will associate with the bot soon.`, ephemeral:true});
-
+        await interaction.followUp({ content:`You can now access the TH11 tournament.Someone from staff will associate with the bot soon.`, ephemeral:true});
+      }
+      else{
+        await member.roles.remove(role)
+        await channel.send({content:`Removed TH11 access from you.`, ephemeral:true});
     } catch (error) {
       console.error('Error handling th11 button interaction:', error);
     }
