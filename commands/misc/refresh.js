de4dd76ff241238b28e6426599f710c2b0be12d0
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2023-11-16 15:09:08 +0530

    Create refresh.js

---
+++ commands/misc/refresh.js
@@ -0,0 +1,36 @@
+const {
+  SlashCommandBuilder,
+  EmbedBuilder,
+  PermissionsBitField,
+} = require('discord.js');
+const player = require('../../db/player.js');
+const claim = require('../../db/claim.js')
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('refresh')
+    .setDescription('Add a player image'),
+
+
+  async execute(interaction) {
+    await interaction.deferReply();
+    try {
+      const data = await player.find().toArray();
+      
+      const channel = interaction.client.channels.cache.get('1174574152947081277')
+      for (const item of data) {
+        await new Promise((resolve) => setTimeout(resolve, 450)); // Wait for 0.45 seconds
+        await channel.send(item.toString());
+      }
+    } catch (e) {
+      let err = new EmbedBuilder()
+        .setColor(0xffff11)
+        .setTitle('Error')
+        .setDescription(e.message);
+      console.log(e);
+      await interaction.followUp({
+        content: '',
+        embeds: [err],
+      });
+    }
+  },
+}
