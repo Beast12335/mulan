101b68035102c9ba03290df81bba227ed3e9af95
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2024-01-14 17:26:10 +0530

    Update player-claim.js

--- commands/player/player-claim.js
+++ commands/player/player-claim.js
@@ -12,6 +12,7 @@ const client = new Client()
   data: new SlashCommandBuilder()
     .setName('player-claim')
     .setDescription('Claim a player')
+    .setDefaultMemberPermissions(PermissionsBitField.Flags.ManageNickname)
     .addStringOption((option) =>
       option
         .setName('tag')
