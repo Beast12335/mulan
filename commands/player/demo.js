c06ae6b766a92ab023fd63436b8684d95e4a5fdb
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2024-02-02 17:20:15 +0530

    Create demo.js

---
+++ commands/player/demo.js
@@ -0,0 +1,96 @@
+const {
+  SlashCommandBuilder,
+  EmbedBuilder,
+  PermissionsBitField,
+} = require('discord.js');
+const player = require('../../db/player.js');
+const claim = require('../../db/claim.js')
+const AWS = require('aws-sdk');
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: 'ap-southeast-1', // replace with your AWS region
+});
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('demo')
+    .setDescription('Add a player image')
+    .addStringOption((option) =>
+      option
+        .setName('tag')
+        .setDescription('Choose the player')
+        .setRequired(true)
+        .setAutocomplete(true)
+    )
+    .addAttachmentOption((option) =>
+      option
+        .setName('image')
+        .setDescription('Upload the player picture')
+        .setRequired(true)
+    ),
+
+  async autocomplete(interaction) {
+    const focusedValue = interaction.options.getFocused();
+    let t = await claim.find({user: interaction.user.id})
+    const choices = t.map((u)=>{
+      return u.tag })
+    const filtered = choices.filter((choice) =>
+      choice.startsWith(focusedValue)
+    );
+    await interaction.respond(
+      filtered.map((choice) => ({name: choice, value: choice}))
+    );
+  },
+
+  async execute(interaction) {
+    await interaction.deferReply();
+    try {
+      let tag = interaction.options.getString('tag')
+      let img = interaction.options.getAttachment('image').url
+      
+      const s3 = new AWS.S3();
+      
+      const params = {
+        Bucket: 'beast-db',
+        Key: `${tag}.jpg`,
+        Body: Buffer.from(img),
+        ContentType: 'image/jpeg',
+        ACL: 'public-read',
+      };
+      
+      s3.upload(params, (err, data) => {
+        if (err) {
+          console.error(err);
+          // Handle error response to the user
+        } else {
+          console.log(`Image uploaded successfully. URL: ${data}`);
+          // Save data.Location (S3 URL) in your MongoDB for future retrieval
+        }
+      });
+      
+      let embed = new EmbedBuilder()
+        .setColor(0xffff00)
+        .setTitle('Sucess')
+        .setDescription('Player picture saved successfully.');
+      const channel = interaction.client.channels.cache.get('1174574152947081277')
+      await channel.send({
+        content:`${tag} \n Added by: ${interaction.user.id} \n Image: `,
+        });
+      await interaction.followUp({
+        content: '',
+        embeds: [embed],
+      });
+    } catch (e) {
+      let err = new EmbedBuilder()
+        .setColor(0xffff11)
+        .setTitle('Error')
+        .setDescription(e.message);
+      console.log(e);
+      await interaction.followUp({
+        content: '',
+        embeds: [err],
+      });
+    }
+  },
+}
