43a3d93da6cd32fde77ed3ab0beb3078fa5b364c
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2024-02-04 13:41:47 +0530

    Update demo.js

--- commands/player/demo.js
+++ commands/player/demo.js
@@ -2,11 +2,22 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const { AttachmentBuilder } = require('discord.js');
 const https = require('https');
 const fs = require('fs');
+const AWS = require('aws-sdk');
+
+AWS.config.update({
+  accessKeyId: process.env.ACCESS_KEY_ID,
+  secretAccessKey: process.env.SECRET_ACCESS_KEY,
+  region: 'ap-southeast-1', // replace with your AWS region
+});
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('storeimage')
     .setDescription('Store the uploaded image')
+    .addStringOption((option) =>
+    option.setName('tag')
+    .setDescription('Enter the player tag')
+    .setRequired(true))
     .addAttachmentOption((option) =>
       option
         .setName('image')
@@ -19,21 +30,23 @@ module.exports = {
 
     try {
       const imageAttachment = interaction.options.getAttachment('image');
-
+      const tag = interaction.options.getString('tag')
       // Fetch the image and convert it to a buffer
       const imageBuffer = await getImageBuffer(imageAttachment.url);
 
       // Generate a unique filename based on timestamp
       const timestamp = new Date().toISOString().replace(/:/g, '-');
-      const filename = `uploaded_image_${timestamp}.jpg`;
+      const filename = `${tag}.jpg`;
 
       // Save the image locally
       fs.writeFileSync(filename, imageBuffer);
 
+      // Upload the image to S3
+      await uploadToS3(filename);
+
       // Respond with a confirmation message and the saved image
       await interaction.followUp({
-        content: `Image stored as \`${filename}\`.`,
-        files: [new AttachmentBuilder(filename)],
+        content: `Player picture saved.`, // Adjust the message as needed
       });
 
       // Optionally, you can perform additional actions with the saved image.
@@ -66,4 +79,18 @@ async function getImageBuffer(url) {
       });
     });
   });
+}
+
+// Helper function to upload the image to AWS S3
+async function uploadToS3(filename) {
+  const s3 = new AWS.S3();
+  const fileContent = fs.readFileSync(filename);
+
+  const params = {
+    Bucket: 'beast-db',
+    Key: filename,
+    Body: fileContent,
+  };
+
+  await s3.upload(params).promise();
 }
