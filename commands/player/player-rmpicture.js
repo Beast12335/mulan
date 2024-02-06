cb459ba0dcf2b03f1d3205b8f84bdcf6d0df8576
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2024-02-06 23:19:38 +0530

    Update player-rmpicture.js

--- commands/player/player-rmpicture.js
+++ commands/player/player-rmpicture.js
@@ -1,9 +1,12 @@
-const {
-  SlashCommandBuilder,
-  EmbedBuilder,
-  PermissionsBitField,
-} = require('discord.js');
-const player = require('../../db/player.js');
+const { SlashCommandBuilder, PermissionsBitField, EmbedBuilder } = require('discord.js');
+const AWS = require('aws-sdk');
+
+AWS.config.update({
+  accessKeyId: process.env.ACCESS_KEY_ID,
+  secretAccessKey: process.env.ACCESS_SECRET_KEY,
+  region: 'ap-southeast-1', // replace with your AWS region
+});
+
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('player-rmpicture')
@@ -16,28 +19,18 @@ module.exports = {
         .setRequired(true)
         .setAutocomplete(true)
     ),
-  async autocomplete(interaction) {
-    const focusedValue = interaction.options.getFocused();
-    const c = await player.find();
-    const choices = c.map((t) => {
-      return t.tag
-    });
-    const filtered = choices.filter((choice) =>
-      choice.startsWith(focusedValue)
-    );
-    await interaction.respond(
-      filtered.map((choice) => ({name: choice, value: choice}))
-    );
-  },
   async execute(interaction) {
     await interaction.deferReply();
     try {
-      let tag = interaction.options.getString('tag')
-      await player.deleteOne({tag:tag})
+      let tag = interaction.options.getString('tag');
+      const file = interaction.options.getString('filename');
+      const filename = `${file}.jpg`;
+      // Delete the image from S3
+      await deleteFromS3(filename);
       let embed = new EmbedBuilder()
-      .setTitle('Sucess')
-      .setColor(0x13ff00)
-      .setDescription(`Picture for ${tag} have been removed`);
+        .setTitle('Success')
+        .setColor(0x13ff00)
+        .setDescription(`Picture for ${tag} has been removed`);
       
       await interaction.followUp({
         content: '',
@@ -55,3 +48,14 @@ module.exports = {
     }
   },
 };
+
+async function deleteFromS3(filename) {
+  const s3 = new AWS.S3();
+
+  const params = {
+    Bucket: 'beast-db',
+    Key: filename,
+  };
+
+  await s3.deleteObject(params).promise();
+}
